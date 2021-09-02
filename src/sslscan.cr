3c41f0d1318e03f3890ea120b9fc68f12ef29f4b
krishpranav <krisna.pranav@gmail.com>
2021-09-02 17:23:55 +0530

    src: sslscan

--- src/sslscan.cr
+++ src/sslscan.cr
@@ -3,36 +3,80 @@ require "socket"
 require "openssl"
 require "./sslscan/*"
 
-
 module SSLScan
-
-    class Scan
-
-        def initialize(ip: String, port: Int32)
-            @ip = ip
-            @port = port
-        end
-
-        def run
-            sort_and_port(gather_results)
-        end
-
-        def ssl_handshake(cipher: Stirng, protocol : Array(OpenSSL::SSL::Options))
-            begin
-                s = socket
-                c = OpenSSL::SSL::Context::Client.new
-                c.ciphers = cipher
-                c.verify_mode = OpenSSL::SSL::VerifyMode::None
-                protocol.each do |options|
-                    c.add_options(option)
-                end
-                OpenSSL::SSL::Socket::Client.new(s, c)
-                s.close if s.is_a? TCPSocket rescue nil
-                true
-            rescue
-                s.close if s.is_a? TCPSocket rescue nil
-                false
-            end
-        end
+  class Scan
+    def initialize(ip : String, port : Int32)
+      @ip = ip
+      @port = port
     end
+
+    def run
+      sort_and_print(gather_results)
+    end
+
+    def gather_results
+      scans = [] of Hash(Symbol, (String | Colorize::Object(String)))
+      results = [] of Int32
+      spawns = [] of Int32
+      SSLScan.protocols.each do |symbol, protocol_bits|
+        SSLScan.ciphers.each do |cipher|
+          spawn do
+            spawns << 0
+            if ssl_handshake(cipher, protocol_bits)
+              eva = SSLScan::Evalutation.new(cipher, symbol)
+              scans << eva.evaluate
+            end
+            results << 0
+          end
+        end
+      end
+      sleep 1
+      until results.size == spawns.size
+        print "\rScanning: #{results.size}/#{spawns.size}"
+        STDOUT.flush
+        sleep 0.1
+      end
+      scans
+    end
+
+    def sort_and_print(results : Array(Hash(Symbol, Colorize::Object(String) | String)))
+      puts "\r\nThe server #{@ip}:#{@port} supports those ciphers and protocols".colorize(:blue)
+
+      SSLScan.protocols.each do |symbol, protocol_bits|
+        results.each do |result|
+          if result[:protocol] == symbol.to_s
+            puts "#{result[:protocol]} -- #{result[:cipher]} -- #{result[:bits]} -- #{result[:strength]} -- #{result[:issues]}"
+          else
+            next
+          end
+        end
+      end
+    end
+
+    def socket
+      begin
+        TCPSocket.new(@ip, @port)
+      rescue e : Exception
+        raise "Error connecting to target #{@ip}:#{@port} : #{e}"
+      end
+    end
+
+    def ssl_handshake(cipher : String, protocol : Array(OpenSSL::SSL::Options))
+      begin
+        s = socket
+        c = OpenSSL::SSL::Context::Client.new
+        c.ciphers = cipher
+        c.verify_mode = OpenSSL::SSL::VerifyMode::NONE
+        protocol.each do |option|
+          c.add_options(option)
+        end
+        OpenSSL::SSL::Socket::Client.new(s, c)
+        s.close if s.is_a? TCPSocket rescue nil
+        true
+      rescue
+        s.close if s.is_a? TCPSocket rescue nil
+        false
+      end
+    end
+  end
 end
